c43208c23b895993ca929638856df5a5b001fcc2
bandicode <vincent.chambrin@gmail.com>
2020-10-27 18:14:49 +0100

    add MetaObject & MetaProperty

--- include/dom/element.h
+++ include/dom/element.h
@@ -13,16 +13,264 @@
 namespace dom
 {
 
+class DOM_API MetaProperty
+{
+public: 
+  std::string name;
+
+public:
+  explicit MetaProperty(std::string name);
+  virtual ~MetaProperty();
+
+  virtual bool readBool(const Node& n) const = 0;
+  virtual int readInt(const Node& n) const = 0;
+  virtual double readDouble(const Node& n) const = 0;
+  virtual std::string readString(const Node& n) const = 0;
+
+  static bool read_bool(bool val);
+  static bool read_bool(int val);
+  static bool read_bool(double val);
+  static bool read_bool(const std::string& val);
+
+  static int read_int(bool val);
+  static int read_int(int val);
+  static int read_int(double val);
+  static int read_int(const std::string& val);
+
+  static double read_double(bool val);
+  static double read_double(int val);
+  static double read_double(double val);
+  static double read_double(const std::string& val);
+
+  static std::string read_string(bool val);
+  static std::string read_string(int val);
+  static std::string read_string(double val);
+  static std::string read_string(const std::string& val);
+
+  virtual void set(Node& n, bool val) = 0;
+  virtual void set(Node& n, int val) = 0;
+  virtual void set(Node& n, double val) = 0;
+  virtual void set(Node& n, std::string val) = 0;
+
+  static void set_property(bool& p, bool val);
+  static void set_property(bool& p, const std::string& val);
+
+  static void set_property(int& p, int val);
+  static void set_property(int& p, const std::string& val);
+
+  static void set_property(double& p, double val);
+  static void set_property(double& p, const std::string& val);
+
+  static void set_property(std::string& p, bool val);
+  static void set_property(std::string& p, int val);
+  static void set_property(std::string& p, double val);
+  static void set_property(std::string& p, std::string val);
+
+  template<typename T>
+  struct Selector
+  {
+
+  };
+
+  bool read(const Node& n, Selector<bool>) const;
+  int read(const Node& n, Selector<int>) const;
+  double read(const Node& n, Selector<double>) const;
+  std::string read(const Node& n, Selector<std::string>) const;
+};
+
+class DOM_API MetaObject
+{
+public:
+  MetaObject* base;
+  std::vector<MetaProperty*> properties;
+
+public:
+  explicit MetaObject(MetaObject* b = nullptr);
+  virtual ~MetaObject();
+
+  MetaProperty* at(size_t index) const;
+  MetaProperty* find(const std::string& name) const;
+
+  size_t size() const;
+};
+
+template<typename C, typename T>
+struct GenericMetaProperty : public MetaProperty
+{
+  T C::* pointer = nullptr;
+
+  GenericMetaProperty(std::string n, T C::* ptr)
+    : MetaProperty(std::move(n)),
+      pointer(ptr)
+  {
+
+  }
+
+  bool readBool(const Node& n) const override
+  {
+    return read_bool((static_cast<const C&>(n)).*pointer);
+  }
+
+  int readInt(const Node& n) const override
+  {
+    return read_int((static_cast<const C&>(n)).*pointer);
+  }
+
+  double readDouble(const Node& n) const override
+  {
+    return read_double((static_cast<const C&>(n)).*pointer);
+  }
+
+  std::string readString(const Node& n) const override
+  {
+    return read_string((static_cast<const C&>(n)).*pointer);
+  }
+
+  void set(Node& n, bool val) override
+  {
+    set_property((static_cast<C&>(n)).*pointer, val);
+  }
+
+  void set(Node& n, int val) override
+  {
+    set_property((static_cast<C&>(n)).*pointer, val);
+  }
+
+  void set(Node& n, double val) override
+  {
+    set_property((static_cast<C&>(n)).*pointer, val);
+  }
+
+  void set(Node& n, std::string val) override
+  {
+    set_property((static_cast<C&>(n)).*pointer, val);
+  }
+};
+
+inline void fill_property_vec_impl(std::vector<MetaProperty*>&)
+{
+
+}
+
+template<typename T, typename... Args>
+void fill_property_vec_impl(std::vector<MetaProperty*>& list, T& prop, Args&... rest)
+{
+  list.push_back(&prop);
+  fill_property_vec_impl(list, std::forward<Args>(rest)...);
+}
+
+template<typename Tuple, std::size_t... Is>
+void fill_property_vec_impl(std::vector<MetaProperty*>& list, Tuple& t, std::index_sequence<Is...>)
+{
+  fill_property_vec_impl(list, std::get<Is>(t)...);
+}
+
+template<typename... Args>
+void fill_property_vec(std::vector<MetaProperty*>& list, std::tuple<Args...>& t)
+{
+  fill_property_vec_impl(list, t, std::index_sequence_for<Args...>{});
+}
+
+template<typename T, typename... Args>
+class GenericMetaObject : public MetaObject
+{
+public:
+  std::tuple<GenericMetaProperty<T, Args>...> property_tuple;
+
+  GenericMetaObject(std::tuple<GenericMetaProperty<T, Args>...> props, MetaObject* p = nullptr)
+    : MetaObject(p),
+      property_tuple(props)
+  {
+    fill_property_vec(properties, property_tuple);
+  }
+};
+
+class AttributeList;
+
 class DOM_API Element : public Node
 {
 public:
   std::string id;
-  std::map<std::string, std::shared_ptr<Node>> attributes;
 
 public:
   using Node::Node;
 
   bool isElement() const override;
+
+  AttributeList attributes() const;
+
+  template<typename T>
+  T getAttribute(const std::string& name) const
+  {
+    MetaProperty* prop = metaObject()->find(name);
+    
+    if (prop)
+      return prop->read(*this, MetaProperty::Selector<T>());
+    return T();
+  }
+
+  // C++17
+  // std::variant<bool, int, double, std::string> getAttribute(const std::string& name);
+
+  void setAttribute(const std::string& name, bool value);
+  void setAttribute(const std::string& name, int value);
+  void setAttribute(const std::string& name, double value);
+  void setAttribute(const std::string& name, std::string value);
+
+protected:
+  friend class AttributeList;
+  static GenericMetaObject<Element, std::string> staticMetaObject;
+  virtual MetaObject* metaObject() const;
+};
+
+class DOM_API Attribute
+{
+public:
+  explicit Attribute(const Element& e, MetaProperty* prop)
+    : element(e),
+      property(prop)
+  {
+
+  }
+
+  template<typename T>
+  T get() const
+  {
+    if (property)
+      return property->read(element, MetaProperty::Selector<T>());
+    else
+      return T();
+  }
+
+private:
+  const Element& element;
+  MetaProperty* property;
+};
+
+class DOM_API AttributeList
+{
+public:
+  explicit AttributeList(const Element& e, MetaObject* mo)
+    : element(e),
+      metaobject(mo)
+  {
+
+  }
+
+  Attribute at(size_t index) const
+  {
+    MetaProperty* prop = element.metaObject()->at(index);
+    return Attribute(element, prop);
+  }
+
+  size_t size() const
+  {
+    return metaobject->size();
+  }
+
+private:
+  const Element& element;
+  MetaObject* metaobject;
 };
 
 } // namespace dom
--- src/element.cpp
+++ src/element.cpp
@@ -4,12 +4,289 @@
 
 #include "dom/element.h"
 
+#include <stdexcept>
+
 namespace dom
 {
+
+GenericMetaObject<Element, std::string> Element::staticMetaObject = {
+  {{"id", &Element::id}}, nullptr
+};
+
+MetaProperty::MetaProperty(std::string n)
+  : name(std::move(n))
+{
+
+}
+
+MetaProperty::~MetaProperty()
+{
+
+}
+
+bool MetaProperty::read_bool(bool val)
+{
+  return val;
+}
+
+bool MetaProperty::read_bool(int val)
+{
+  throw std::runtime_error{ "dom get attribute error" };
+}
+
+bool MetaProperty::read_bool(double val)
+{
+  throw std::runtime_error{ "dom get attribute error" };
+}
+
+bool MetaProperty::read_bool(const std::string& val)
+{
+  throw std::runtime_error{ "dom get attribute error" };
+}
+
+
+int MetaProperty::read_int(bool val)
+{
+  throw std::runtime_error{ "dom get attribute error" };
+}
+
+int MetaProperty::read_int(int val)
+{
+  return val;
+}
+
+int MetaProperty::read_int(double val)
+{
+  throw std::runtime_error{ "dom get attribute error" };
+}
+
+int MetaProperty::read_int(const std::string& val)
+{
+  throw std::runtime_error{ "dom get attribute error" };
+}
+
+
+double MetaProperty::read_double(bool val)
+{
+  throw std::runtime_error{ "dom get attribute error" };
+}
+
+double MetaProperty::read_double(int val)
+{
+  throw std::runtime_error{ "dom get attribute error" };
+}
+
+double MetaProperty::read_double(double val)
+{
+  return val;
+}
+
+double MetaProperty::read_double(const std::string& val)
+{
+  throw std::runtime_error{ "dom get attribute error" };
+}
+
+
+std::string MetaProperty::read_string(bool val)
+{
+  throw std::runtime_error{ "dom get attribute error" };
+}
+
+std::string MetaProperty::read_string(int val)
+{
+  throw std::runtime_error{ "dom get attribute error" };
+}
+
+std::string MetaProperty::read_string(double val)
+{
+  throw std::runtime_error{ "dom get attribute error" };
+}
+
+std::string MetaProperty::read_string(const std::string& val)
+{
+  return val;
+}
+
+
+void MetaProperty::set_property(bool& p, bool val)
+{
+  p = val;
+}
+
+void MetaProperty::set_property(bool& p, const std::string& val)
+{
+  p = (val == "true" || val == "True" || val == "TRUE");
+}
+
+void MetaProperty::set_property(int& p, int val)
+{
+  p = val;
+}
+
+void MetaProperty::set_property(int& p, const std::string& val)
+{
+  p = std::stoi(val);
+}
+
+void MetaProperty::set_property(double& p, double val)
+{
+  p = val;
+}
+
+void MetaProperty::set_property(double& p, const std::string& val)
+{
+  p = std::stod(val);
+}
+
+void MetaProperty::set_property(std::string& p, bool val)
+{
+  p = val ? "true" : "false";
+}
+
+void MetaProperty::set_property(std::string& p, int val)
+{
+  p = std::to_string(val);
+}
+
+void MetaProperty::set_property(std::string& p, double val)
+{
+  p = std::to_string(val);
+}
+
+void MetaProperty::set_property(std::string& p, std::string val)
+{
+  p = val;
+}
+
+bool MetaProperty::read(const Node& n, Selector<bool>) const
+{
+  return readBool(n);
+}
+
+int MetaProperty::read(const Node& n, Selector<int>) const 
+{
+  return readInt(n);
+}
+
+double MetaProperty::read(const Node& n, Selector<double>) const 
+{
+  return readDouble(n);
+}
+
+std::string MetaProperty::read(const Node& n, Selector<std::string>) const
+{
+  return readString(n);
+}
+
+MetaObject::MetaObject(MetaObject* b)
+  : base(b)
+{
+
+}
+
+MetaObject::~MetaObject()
+{
+
+}
+
+MetaProperty* MetaObject::at(size_t index) const
+{
+  if (base)
+  {
+    size_t s = base->size();
+
+    if (index < s)
+      return base->at(index);
+    else
+      index -= s;
+  }
+
+  if (index < properties.size())
+    return properties.at(index);
+  else
+    return nullptr;
+}
+
+MetaProperty* MetaObject::find(const std::string& name) const
+{
+  if (base)
+  {
+    auto result = base->find(name);
+
+    if (result)
+      return result;
+  }
+
+  for (MetaProperty* p : properties)
+  {
+    if (p->name == name)
+      return p;
+  }
+
+  return nullptr;
+}
+
+size_t MetaObject::size() const
+{
+  if (base)
+    return base->size() + properties.size();
+  else
+    return properties.size();
+}
 
 bool Element::isElement() const
 {
   return true;
+}
+
+AttributeList Element::attributes() const
+{
+  return AttributeList(*this, metaObject());
+}
+
+void Element::setAttribute(const std::string& name, bool value)
+{
+  MetaProperty* p = metaObject()->find(name);
+
+  if (p)
+  {
+    p->set(*this, value);
+  }
+}
+
+void Element::setAttribute(const std::string& name, int value)
+{
+  MetaProperty* p = metaObject()->find(name);
+
+  if (p)
+  {
+    p->set(*this, value);
+  }
+}
+
+void Element::setAttribute(const std::string& name, double value)
+{
+  MetaProperty* p = metaObject()->find(name);
+
+  if (p)
+  {
+    p->set(*this, value);
+  }
+}
+
+void Element::setAttribute(const std::string& name, std::string value)
+{
+  MetaProperty* p = metaObject()->find(name);
+
+  if (p)
+  {
+    p->set(*this, value);
+  }
+}
+
+MetaObject* Element::metaObject() const
+{
+  return &staticMetaObject;
 }
 
 } // namespace dom
