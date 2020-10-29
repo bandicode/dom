1ddb8143aaf0984e2fb26e9d5614ee3eeee539be
bandicode <vincent.chambrin@gmail.com>
2020-10-29 19:34:44 +0100

    update attributes of Document class

--- include/dom/document.h
+++ include/dom/document.h
@@ -16,6 +16,10 @@ namespace dom
 class DOM_API Document : public Node
 {
 public:
+  std::string title;
+  std::string doctype = "document";
+
+public:
   ~Document() = default;
 
   static const std::string Type;
@@ -23,16 +27,7 @@ public:
 
   explicit Document(std::string title = "");
 
-  const std::string& title() const;
-  const std::string& author() const;
-  const std::string& date() const;
-  int revision() const;
   const std::vector<std::shared_ptr<Node>>& nodes() const;
-
-  void setTitle(std::string str);
-  void setAuthor(std::string str);
-  void setDate(std::string str);
-  void setRevision(int rev);
 
   void addNode(std::shared_ptr<Node> n);
   void insertNode(size_t index, std::shared_ptr<Node> n);
@@ -46,10 +41,6 @@ public:
   void swap(size_t i, size_t j);
 
 private:
-  std::string m_title;
-  std::string m_author;
-  std::string m_date;
-  int m_revision = -1;
   std::vector<std::shared_ptr<Node>> m_nodes;
 };
 
@@ -58,55 +49,15 @@ private:
 namespace dom
 {
 
-Document::Document(std::string title)
-  : m_title(std::move(title))
+Document::Document(std::string tt)
+  : title(std::move(tt))
 {
 
-}
-
-inline const std::string& Document::title() const
-{
-  return m_title;
-}
-
-inline const std::string& Document::author() const
-{
-  return m_author;
-}
-
-inline const std::string& Document::date() const
-{
-  return m_date;
-}
-
-inline int Document::revision() const
-{
-  return m_revision;
 }
 
 inline const std::vector<std::shared_ptr<Node>>& Document::nodes() const
 {
   return m_nodes;
-}
-
-inline void Document::setTitle(std::string str)
-{
-  m_title = std::move(str);
-}
-
-inline void Document::setAuthor(std::string str)
-{
-  m_author = std::move(str);
-}
-
-inline void Document::setDate(std::string str)
-{
-  m_date = std::move(str);
-}
-
-inline void Document::setRevision(int rev)
-{
-  m_revision = rev;
 }
 
 } // namespace dom
